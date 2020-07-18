99cb69cb629bcb29f44137de4b6b31263fe8cdc2
Sergey Ganzha <38720631+GanzhaSergey@users.noreply.github.com>
2020-07-18 12:31:59 +0300

    Add files via upload

--- works/webDevelop/js/script.js
+++ works/webDevelop/js/script.js
@@ -24,3 +24,10 @@ $('.latestWorks__rowItem').click(function() {
     $(this).addClass('latestWorks__rowItem-active');
 
 })
+
+
+// burger menu
+$('.header__burger').click(function(event) {
+    $('.header__burger, .header__menu').toggleClass('active');
+    $('body').toggleClass('lock');
+  });
