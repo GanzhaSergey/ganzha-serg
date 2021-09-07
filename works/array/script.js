d36c8667cc4a256dd8e1e3d9a59a45619e147331
Sergey Ganzha <38720631+GanzhaSergey@users.noreply.github.com>
2021-09-07 14:02:19 +0300

    Add files via upload

--- works/array/script.js
+++ works/array/script.js
@@ -1,13 +1,18 @@
-document.querySelector("#but").onclick = function () {
-let len = document.querySelector("#length").value;
-let min = document.querySelector("#min").value;
-let max = document.querySelector("#max").value;
-let result = [];
 
-for (let i = 0; i < len; i++) {
-result.push(randomInteger(min,max));
+
+document.querySelector("#but").onclick = function () {
+  $('#out').empty();
+  let len = document.querySelector("#length").value;
+  let min = document.querySelector("#min").value;
+  let max = document.querySelector("#max").value;
+  let result = [];
+
+    for (let i = 0; i < len; i++) {
+    result.push(randomInteger(min,max));
+    let div = $("<div>").html("Array element[" + [i+1] + "]:  " + result[i]);
+    $('#out').append(div);  
     }
-document.querySelector('#out').innerHTML = "Generate array: " + result;
+
 }
 
 function randomInteger(min, max) {
