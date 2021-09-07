7b4630aea46f1b9f6a1b3d5bbd600cc99ef6b961
Sergey Ganzha <38720631+GanzhaSergey@users.noreply.github.com>
2021-09-07 12:51:41 +0300

    Add files via upload

--- works/lpgcalc/script.js
+++ works/lpgcalc/script.js
@@ -27,30 +27,39 @@ document.querySelector(".inp44").onclick = function() {
     document.querySelector("#gasPrice").focus();
 }
 
-document.querySelector("#benzcon").onchange = Res;
-document.querySelector("#mouthRun").onchange = Res;
-document.querySelector("#benzPrice").onchange = Res;
-document.querySelector("#gasPrice").onchange = Res;
+// document.querySelector("#benzcon").onchange = Res;
+// document.querySelector("#mouthRun").onchange = Res;
+// document.querySelector("#benzPrice").onchange = Res;
+// document.querySelector("#gasPrice").onchange = Res;
 
 
-document.querySelector("#res").onclick = Res;
 
-    function Res() {
-            let benzincons = document.querySelector('#benzcon').value;
-            let gascons = benzincons * 1.1;
-            let benzprice = document.querySelector('#benzPrice').value * benzincons;
-            let gasprice = document.querySelector('#gasPrice').value * gascons;
-            let econ = benzprice - gasprice;
-            document.querySelector('#pricebenz').innerHTML = "Стоимость 100км на бензине: " + benzprice + " грн.";
-            document.querySelector('#pricegas').innerHTML = "Стоимость 100км на газу: " + gasprice + " грн.";
-            document.querySelector('#econ').innerHTML = "Экономия на 100км составляет: " + econ + " грн.";
-            let mountRun = document.querySelector('#mouthRun').value;
-            
-            let econMounthBenz = benzprice * (mountRun/100);
-            let econMounthGas = gasprice * (mountRun/100);
-            let econMounth = econMounthBenz - econMounthGas;
-            document.querySelector('#econMounthBenz').innerHTML = "Стоимость месяца на бензине: " + econMounthBenz + " грн.";
-            document.querySelector('#econMounthGas').innerHTML = "Стоимость месяца на газу: " + econMounthGas + " грн.";
-            document.querySelector('#econMounth').innerHTML = "Экономия за месяц составляет: " + econMounth + " грн.";
-      
-}
+$('#res').on('click', function() {
+    let benzincons = document.querySelector('#benzcon').value;
+    let benzPrice = document.querySelector('#benzPrice').value;
+    let gasPrice = document.querySelector('#gasPrice').value;
+    let mountRun = document.querySelector('#mouthRun').value;
+
+            if ((benzincons === '') || (benzPrice === '') || (gasPrice === '') | (mountRun === '')) alert("Не заполнено одно из полей!!!");
+            else 
+            {
+                    
+                    let gascons = benzincons * 1.1;
+                    let benzprice = benzPrice * benzincons;
+                    let gasprice = gasPrice * gascons;
+                    let econ = benzprice - gasprice;
+                    document.querySelector('#pricebenz').innerHTML = "Стоимость 100км на бензине: " + benzprice + " грн.";
+                    document.querySelector('#pricegas').innerHTML = "Стоимость 100км на газу: " + gasprice + " грн.";
+                    document.querySelector('#econ').innerHTML = "Экономия на 100км составляет: " + econ + " грн.";
+                
+                    
+                    let econMounthBenz = benzprice * (mountRun/100);
+                    let econMounthGas = gasprice * (mountRun/100);
+                    let econMounth = econMounthBenz - econMounthGas;
+                    document.querySelector('#econMounthBenz').innerHTML = "Стоимость месяца на бензине: " + econMounthBenz + " грн.";
+                    document.querySelector('#econMounthGas').innerHTML = "Стоимость месяца на газу: " + econMounthGas + " грн.";
+                    document.querySelector('#econMounth').innerHTML = "Экономия за месяц составляет: " + econMounth + " грн.";
+            }
+          
+        }
+);
